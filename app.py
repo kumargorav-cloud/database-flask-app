b8c3ad37686d3005a9fd7226240fc658005fe46b
kumargorav-cloud <singhalanhilom145@gmail.com>
2026-04-16 12:20:56 +0000

    created db

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-db=SQLAlchemy()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__,template_folder='templates')
+    
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
 
     db.init_app(app)
 
     from routes import register_routes
-    register_routes(app,db)
+    register_routes(app, db)
     # after doing we will run in terminal
     # flask db init --to initialize the db
     #flask db migrate--to send the table 
